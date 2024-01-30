6c60b20676a16b02815d4d4189f186e1d190dda4
Subrat kumar jena <subratj611@gmail.com>
2024-01-31 00:16:36 +0530

    tailwindcss config setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,ts}"],
+  content: ["./*.{html,js}", "./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
